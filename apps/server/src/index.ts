f66d90c74f47f569a3cfe7c9980528391447cabe
Prit Patel <projects.pritpatel@gmail.com>
2026-06-19 22:14:33 +0530

    better auth configured

---
+++ apps/server/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.emit("welcome", "Connected to server");
+
+  socket.on("message", (data) => {
+    console.log("Received:", data);
+
+    // send to all connected clients
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket.IO Server Running");
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
